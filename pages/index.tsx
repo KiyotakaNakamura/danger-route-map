9c7f070489ff8ccf5ceb8ec015e9c2ffd2e6ef6d
KiyotakaNakamura <nakamura@nt-studio.com>
2025-04-16 18:03:23 +0900

    迂回９

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const libraries: ('geometry')[] = ['geometry'];
 
 const dangerPoints = [
   { id: 1, lat: 32.786934469864164, lng: 130.70891539313695 },
-  { id: 2, lat: 32.786874167856624, lng: 130.70987178180764 }
+//  { id: 2, lat: 32.786874167856624, lng: 130.70987178180764 }
 ,
 ];
 
@@ -50,7 +50,7 @@ export default function DangerAvoidMap() {
                   new window.google.maps.LatLng(point.lat(), point.lng()),
                   new window.google.maps.LatLng(danger.lat, danger.lng)
                 );
-                return distance < 20;
+                return distance < 30;
               });
             });
           });
